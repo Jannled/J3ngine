e71daacc81460e600364c67a0e6a0574105d6fad
Jannled <jannled@live.de>
2020-07-15 15:51:27 +0200

    CMake define Git Hash in .h file

--- src/testmain.cpp
+++ src/testmain.cpp
@@ -8,9 +8,9 @@
 
 void main(int argc, char** argv)
 {
-	printf("J3Engine Version %d.%d.%d launched with %d params!\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, argc);
+	printf("J3Engine Version %d.%d.%d %s launched with %d params!\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, GIT_SHORT_HASH, argc);
 
 	printf("Fenster: %zd\n", J3::WindowManager::getWindowCount());
 
-	new J3::Node();
+	//new J3::Node();
 }
